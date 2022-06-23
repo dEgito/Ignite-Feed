165820ade663dc1f0905562953703055cbb8e348
Danielly Egito <egitodany@gmail.com>
2022-06-23 13:38:10 -0300

    feat: adiciona campo de comentário

--- src/components/Post/style.jsx
+++ src/components/Post/style.jsx
@@ -1,68 +1,121 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Article = styled.article`
-    background-color: var(--gray-800);
-    border-radius: 4px;
-    padding: 2.5rem;
+  background-color: var(--gray-800);
+  border-radius: 4px;
+  padding: 2.5rem;
 
-    margin-bottom: 2rem;
+  margin-bottom: 2rem;
 
-    > header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  time {
+    font-size: 0.875rem;
+    color: var(--gray-400);
+  }
+
+  form {
+    width: 100%;
+    margin-top: 1.5rem;
+    padding-top: 1.5rem;
+    border-top: 1px solid var(--gray-600);
+
+    > strong {
+      line-height: 1.6;
+      color: var(--gray-100);
     }
 
-    time {
-        font-size: .875rem;
-        color: var(--gray-400);
+    > textarea {
+      width: 100%;
+      background-color: var(--gray-900);
+      border: 0;
+      resize: none;
+      height: 6rem;
+      padding: 1rem;
+      border-radius: 8px;
+      color: var(--gray-100);
+      line-height: 1.4;
+      margin-top: 1rem;
     }
+
+    footer {
+      visibility: hidden;
+      max-height: 0;
+
+      > button {
+        padding: 1rem 1.5rem;
+        margin-top: 1rem;
+        border-radius: 4px;
+        border: 0;
+        background-color: var(--green-500);
+        color: var(--white);
+        font-weight: bold;
+        cursor: pointer;
+
+        /* display: none; Não é tão interessante por causa da acessibilidade, por isso usar o footer */
+
+        &:hover {
+          background-color: var(--green-300);
+          transition: background-color 0.1s;
+        }
+      }
+    }
+
+    &:focus-within footer {
+      visibility: visible;
+      max-height: none;
+    }
+  }
 `;
 
 export const Content = styled.article`
-    display: flex;
-    flex-direction: column;
-    line-height: 1.6;
-    color: var(--gray-300);
-    margin-top: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  line-height: 1.6;
+  color: var(--gray-300);
+  margin-top: 1.5rem;
 
-    p {
-        margin-top: 1rem;
+  p {
+    margin-top: 1rem;
+  }
+
+  a {
+    font-weight: bold;
+    color: var(--green-500);
+    text-decoration: none;
+    margin: 0 2px;
+
+    &:hover {
+      color: var(--green-300);
     }
-
-    a {
-        font-weight: bold;
-        color: var(--green-500);
-        text-decoration: none;
-        margin: 0 2px;
-
-        &:hover {
-            color: var(--green-300);
-        }
-    }
+  }
 `;
 
 export const Author = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  > div {
     display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1rem;
+    flex-direction: column;
 
-    > div {
-        display: flex;
-        flex-direction: column;
-
-        strong {
-            color: var(--gray-100);
-            line-height: 1.6;
-        }
-
-        span {
-            font-size: .875rem;
-            color: var(--gray-400);
-            line-height: 1.6;
-        }
+    strong {
+      color: var(--gray-100);
+      line-height: 1.6;
     }
+
+    span {
+      font-size: 0.875rem;
+      color: var(--gray-400);
+      line-height: 1.6;
+    }
+  }
 `;
 
 export const Icon = styled.img`
@@ -73,4 +126,3 @@ export const Icon = styled.img`
 
   outline: 2px solid var(--green-500);
 `;
-
