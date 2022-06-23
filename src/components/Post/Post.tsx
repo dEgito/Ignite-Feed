15fd4eb9bbfc57cc25dbec2f76de46fd430b1f83
Danielly Egito <egitodany@gmail.com>
2022-06-22 21:41:29 -0300

    feat: adiciona globalStyle

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,13 @@
+interface PostProps {
+  author: string;
+  content: string;
+}
+
+export function Post(props: PostProps) {
+  return (
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  );
+}
